8daa7f718f731de28bee1de6977dca6b0efc98fe
Gilteun Choi <gilteunchoi@gmail.com>
2021-07-15 17:38:10 +0900

    error response format fixed

--- viss/websocket.py
+++ viss/websocket.py
@@ -14,20 +14,20 @@ def is_request_authorized(json):
     else:
         return False
 
-def validate_token(token):
-    print(token)
-    try:
-        body = jwt.decode(token, SIMPLE_JWT['SIGNING_KEY'], SIMPLE_JWT['ALGORITHM'])
-        print(body)
-        return True
-    except jwt.ExpiredSignatureError:
-        print(jwt.ExpiredSignatureError)
-        return False
-    except jwt.InvalidTokenError:
-        print(jwt.InvalidTokenError)
-        return False
-    else:
-        return False
+# def validate_token(token):
+#     print(token)
+#     try:
+#         body = jwt.decode(token, SIMPLE_JWT['SIGNING_KEY'], SIMPLE_JWT['ALGORITHM'])
+#         print(body)
+#         return True
+#     except jwt.ExpiredSignatureError:
+#         print(jwt.ExpiredSignatureError)
+#         return False
+#     except jwt.InvalidTokenError:
+#         print(jwt.InvalidTokenError)
+#         return False
+#     else:
+#         return False
 
 def action_(json):
     return json["action"]
@@ -47,6 +47,15 @@ def url_path_(json):
         url_path = url_path[0:len(url_path)-1]
     return url_path
 
+def error_response_maker(number, reason, message):
+    new_json = {}
+    new_json["error"] = {}
+    new_json["error"]["number"] = number #WEEK POINT: possible hazard
+    new_json["error"]["reason"] = reason
+    new_json["error"]["message"] = message
+    new_json["ts"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    return new_json
+
 async def accept(websocket, path):
         print("client connected")
         while True:
@@ -64,8 +73,9 @@ async def accept(websocket, path):
             print('A')
             if is_request_authorized(dl):
                 print('B')
-                print(dl["authorization"])
-                if validate_token(dl["authorization"]):
+                try:
+                    body = jwt.decode(dl["authorization"], SIMPLE_JWT['SIGNING_KEY'], SIMPLE_JWT['ALGORITHM'])
+                    print(body)
                     print('C')
                     # token is exist && token is valid
                     if action == 'get':
@@ -89,19 +99,33 @@ async def accept(websocket, path):
                             response_json = update(url_path, vehicle_data, dl)
                     
                     else:
+                        #subscribe
+                        #curve logging subscribe
                         pass
+
+                except jwt.ExpiredSignatureError:
+                    response_json = error_response_maker("401", "token_expired", "Access token has expired.")
+                    
+                except jwt.InvalidTokenError:
+                    response_json = error_response_maker("401", "token_invalid", "Access token is invalid.")
+                    
                 else:
-                    #token is exist && token is not valid
+                    #token_missing
+                    #too_many_attempts
                     pass
+
             else:
                 #token not exist
                 pass
+            
+            if "Error Code" in response_json:
+                response_json = error_response_maker(response_json["Error Code"][0:3], response_json["Error Reason"], response_json["message"])
 
-            #dt = datetime.now()
-            #s = str(dt.microsecond)
-            #response_json = '{"action":"get","requestId":"%s","value":"2372", "timestamp":"%s"}' % (id, s) 
+            final_json = {}
+            final_json["action"] = action
+            final_json["requestId"] = requestId
+            for key in response_json:
+                final_json[key] = response_json[key]
 
-            response_json["action"] = action
-            response_json["requestId"] = requestId
-            print(json.dumps(response_json))
-            await websocket.send(json.dumps(response_json))
+            print(json.dumps(final_json))
+            await websocket.send(json.dumps(final_json))
