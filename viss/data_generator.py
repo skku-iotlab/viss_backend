00ede94ce67e12b1832fe4e9223823f3c20d1de4
Gilteun Choi <gilteunchoi@gmail.com>
2021-07-26 17:53:31 +0900

    minor modification

--- viss/data_generator.py
+++ viss/data_generator.py
@@ -39,16 +39,16 @@ def recursive_json_formatter(json, ts):
     return temp
 
 # GENERATOR : make random dataset based on next conditions; (datatype, min, max, enum)
-def recursive_json_generator(json, ts, granpa):
+def recursive_json_generator(json, ts, granpa, num):
     #json_copy = copy.deepcopy(json)
     json_copy = json
     temp = dict()
     for parent in json_copy:
         child = json_copy[parent]
         if parent in ["children"]:
-            return recursive_json_generator(child, ts, granpa)
+            return recursive_json_generator(child, ts, granpa, num)
         elif isinstance(child, dict):
-            temp[parent] = recursive_json_generator(child, ts, parent)
+            temp[parent] = recursive_json_generator(child, ts, parent, num)
         elif parent in ["datatype"]:
             value_list = []
             min, max, enum = None, None, None 
@@ -61,7 +61,7 @@ def recursive_json_generator(json, ts, granpa):
                     enum = json_copy[rule]
                 else:
                     continue
-            for days_ago in range(0, 8):
+            for days_ago in range(0, num):
                 if enum is not None:
                     if(min is not None): raise Exception('Unhandled Case')
                     elif(max is not None): raise Exception('Unhandled Case')
@@ -127,12 +127,15 @@ def recursive_json_generator(json, ts, granpa):
             continue
     return temp
 
+def merge_datasets(json_new, json_old):
+    pass
+
 def getVehicleData():
     with open('viss/vss_release_2.1.json') as file_origin:
         json_file = json.loads(file_origin.read())
         #print(sorted(recursive_get_all_datatype(json_file)))
         ts = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-        json_file = recursive_json_generator(json_file, ts, '')
+        json_file = recursive_json_generator(json_file, ts, '', 2)
         #pprint.pprint(json_file)
         with open('viss/vss_final.json', 'w') as file_final:
             file_final.write(json.dumps(json_file))
