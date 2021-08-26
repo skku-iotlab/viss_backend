b6e85a2518fbf547bda2e0fb826a24e10e5dfa7e
Gilteun Choi <gilteunchoi@gmail.com>
2021-08-26 16:21:40 +0900

    django throttling error fixed 2

--- testdjango/settings.py
+++ testdjango/settings.py
@@ -82,9 +82,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework.throttling.UserRateThrottle'
-    ),
 }
 
 SIMPLE_JWT = {
