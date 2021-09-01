364518501ea6ea7bda75573484688a47d3a13f1e
juun9714 <lovekwon200@gmail.com>
2021-09-01 21:11:23 +0900

    CORS

--- testdjango/settings.py
+++ testdjango/settings.py
@@ -170,3 +170,9 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000' ,'http://localhost:3003'] 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+#make CORS allows every domain
